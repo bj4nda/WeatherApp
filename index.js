803f881ea9af78b0779eb5be3fe887b4888c9fde
Bharath Jinda <37134385+bj4nda@users.noreply.github.com>
2022-10-12 18:45:19 +0530

    Update index.js

--- index.js
+++ index.js
@@ -40,64 +40,6 @@ let weather = {
      }
 };
 
-let geocode = {
-    reverseGeocode : function(latitude, longitude) {
-        var api_key = 'cd0082e1ac5644539a96e495171de94a';
-      
-        var api_url = 'https://api.opencagedata.com/geocode/v1/json'
-      
-        var request_url = api_url
-          + '?'
-          + 'key=' + api_key
-          + '&q=' + encodeURIComponent(latitude + ',' + longitude)
-          + '&pretty=1'
-          + '&no_annotations=1';
-      
-        // see full list of required and optional parameters:
-        // https://opencagedata.com/api#forward
-      
-        var request = new XMLHttpRequest();
-        request.open('GET', request_url, true);
-      
-        request.onload = function() {
-          // see full list of possible response codes:
-          // https://opencagedata.com/api#codes
-      
-          if (request.status === 200){
-            // Success!
-            var data = JSON.parse(request.responseText);
-            weather.fetchWeather(data.results[0].components.city); // print the location
-      
-          } else if (request.status <= 500){
-            // We reached our target server, but it returned an error
-      
-            console.log("unable to geocode! Response code: " + request.status);
-            var data = JSON.parse(request.responseText);
-            console.log('error msg: ' + data.status.message);
-          } else {
-            console.log("server error");
-          }
-        };
-      
-        request.onerror = function() {
-          // There was a connection error of some sort
-          console.log("unable to connect to server");
-        };
-      
-        request.send();  // make the reques
-      },
-
-      getLocation: function () { 
-        function success(data) {
-            geocode.reverseGeocode(data.coords.latitude, data.coords.longitude);
-        }
-        if(navigator.geolocation){
-            navigator.geolocation.getCurrentPosition(success, console.error);
-        } else {
-            weather.fetchWeather("Chennai");
-        }
-      }
-}
 
 
 document.querySelector(".search button").addEventListener("click", function() {
@@ -110,5 +52,6 @@ document.querySelector(".search-bar").addEventListener("keyup", function(event) 
     }
 });
 
+weather.fetchWeather("Chennai");
 
-geocode.getLocation();
+
