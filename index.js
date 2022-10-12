f88828b6ec702d42606a0a7cb7731a54c981fc62
Bharath Jinda <37134385+bj4nda@users.noreply.github.com>
2022-10-12 18:34:53 +0530

    Update index.js

--- index.js
+++ index.js
@@ -112,10 +112,3 @@ document.querySelector(".search-bar").addEventListener("keyup", function(event) 
 
 
 geocode.getLocation();
-
-git init                                                           // start tracking current directory
-git add -A                                                         // add all files in current directory to staging area, making them available for commit
-git commit -m "commit message"                                     // commit your changes
-git remote add origin https://github.com/bj4nda/repo-name.git    // add remote repository URL which contains the required details
-git pull origin master                                             // always pull from remote before pushing
-git push -u origin master                                          
